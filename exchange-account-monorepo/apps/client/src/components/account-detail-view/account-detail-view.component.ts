ae3d0a4b5cab3fa890fd34f80886a272b67a998e
AlbertoMartinNot <albertomartinnot@gmail.com>
2023-10-17 08:31:10 +0200

    add material header, card for title and breadcrumbs and material table, create account detail component

---
+++ exchange-account-monorepo/apps/client/src/components/account-detail-view/account-detail-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'exchange-account-monorepo-account-detail-view',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './account-detail-view.component.html',
+  styleUrls: ['./account-detail-view.component.css'],
+})
+export class AccountDetailViewComponent {}
